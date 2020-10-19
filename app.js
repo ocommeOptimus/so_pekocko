28b6445b393717de2e111ee4d88c9fd54d50b2a4
JeremyBoisdur <jeremy.boisdur@digitregroup.com>
2020-10-19 20:11:32 +0200

    Added the first request POST

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -7,25 +8,15 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/sauces', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Sauce ajoutée !'
+    });
   });
-
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre f*cking requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
 
 module.exports = app;
