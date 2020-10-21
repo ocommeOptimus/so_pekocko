f9aba265768b690ec6c2bf102e6d08be0056327b
JeremyBoisdur <jeremy.boisdur@digitregroup.com>
2020-10-21 14:13:33 +0200

    Added GET, PUT & DELETE request

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const Sauce = require('./models/sauce');
+
 const app = express();
 
 mongoose.connect('mongodb+srv://ocommeoptimus:SdOG5qCmq5toXv59@cluster0.zes4b.mongodb.net/so_pekocko?retryWrites=true&w=majority',
@@ -20,10 +22,37 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.post('/api/sauces', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Sauce ajoutée !'
+    delete req.body._id;
+    const sauce = new Sauce({
+      ...req.body
     });
-  });
+    sauce.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+});
+
+app.put('/api/sauces/:id', (req, res, next) => {
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/sauces/:id', (req, res, next) => {
+  Sauce.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/sauces/:id', (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.use('/api/sauces', (req, res, next) => {
+  Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
+});
 
 module.exports = app;
