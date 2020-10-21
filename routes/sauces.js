46231740deef6a5d7ccff8738ac13759ed305831
JeremyBoisdur <jeremy.boisdur@digitregroup.com>
2020-10-21 20:16:50 +0200

    Updated routes
    with authentication

--- routes/sauces.js
+++ routes/sauces.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const saucesCtrl = require('../controllers/sauces');
 
-router.post('/', saucesCtrl.createThing);
-router.get('/:id', saucesCtrl.getOneThing);
-router.put('/:id', saucesCtrl.modifyThing);
-router.delete('/:id', saucesCtrl.deleteThing);
-router.get('/', saucesCtrl.getAllStuff);
+router.post('/', auth, saucesCtrl.createThing);
+router.get('/:id', auth, saucesCtrl.getOneThing);
+router.put('/:id', auth, saucesCtrl.modifyThing);
+router.delete('/:id', auth, saucesCtrl.deleteThing);
+router.get('/', auth, saucesCtrl.getAllStuff);
 
 module.exports = router;
