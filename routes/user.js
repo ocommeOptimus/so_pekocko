d50cbba5be66e42c92899c2f6911f0027d7a7f0a
JeremyBoisdur <jeremy.boisdur@digitregroup.com>
2020-11-13 17:10:06 +0100

    Applied input validation

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
 
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', [
+  body('email').isEmail(),
+  body('password').isLength({ min: 5 })
+], userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
